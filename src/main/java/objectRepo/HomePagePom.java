1f2ac0ef912489ab5218415c59399f6547ad96ab
devee <devee@DESKTOP-93RQOLS>
2024-03-13 11:20:58 +0530

    Commit 1

---
+++ src/main/java/objectRepo/HomePagePom.java
@@ -0,0 +1,9 @@
+package objectRepo;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePagePom {
+     @FindBy(xpath = "")
+     private WebElement somElement;
+}
---
+++ src/test/java/testRepo/SearchPdt.java
@@ -0,0 +1,12 @@
+package testRepo;
+
+import org.testng.annotations.Test;
+
+import genericUtility.BaseClass;
+
+public class SearchPdt extends BaseClass{
+    @Test
+    public void searchPdt () {
+    	
+    }
+} 
